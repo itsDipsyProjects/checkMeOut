15d4092ffe06afa5f12e46a8a17155b17b7bc2f3
Elliot Collins <150339930+itsDipsyProjects@users.noreply.github.com>
2025-07-30 12:49:35 +0200

    Fixed some css too the navbar so it is on the right hand side when clicked.

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,11 +1,43 @@
 import "./NavBar.css"
+import { useState } from 'react';
 
-export let NavBar = function(){
-    return (
-        <div className="NavBar">
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
-    )
-}
+export let NavBar = function() {
+    const [onClickBool, setClickValue] = useState(true);
+
+    function toggleNavBar() {
+        setClickValue(false);
+    }
+    
+
+    if (onClickBool) {
+        return (
+            <div onClick={toggleNavBar} className="NavBar">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        );
+    } else {
+        return (
+            <div className="NavBarClicked">
+                <div className="exitBtn"></div>
+                <div className="linksContainer">
+                    <ul>
+                        <li>
+                            <a href="">Home</a>
+                        </li>
+                        <li>
+                            <a href="">Work</a>
+                        </li>
+                        <li>
+                            <a href="">About</a>
+                        </li>
+                        <li>
+                            <a href="">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+};
