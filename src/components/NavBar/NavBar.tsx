4855c1b7c83fdf86c918f74275daf82397845564
Elliot Collins <150339930+itsDipsyProjects@users.noreply.github.com>
2025-08-01 15:11:34 +0200

    tweaked some CSS and functionality in the NavBar Component

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,43 +1,69 @@
-import "./NavBar.css"
-import { useState } from 'react';
+import "./NavBar.css";
+import { useState } from "react";
+import { useRef } from "react";
 
-export let NavBar = function() {
-    const [onClickBool, setClickValue] = useState(true);
+export let NavBar = function () {
+  let navBarRef = useRef(null);
 
-    function toggleNavBar() {
-        setClickValue(false);
-    }
+  
+  const [onClickBool, setClickValue] = useState(true);
+  const [animateClose, setAnimateClose] = useState(false);
+
+  function toggleOnNavBar() {
+    setClickValue(false);
+  }
+
+  function toggleOffNavBar() {
+    setAnimateClose(true); // trigger slideOut
+
+    let setTimeoutID = setTimeout(() => {
+        setClickValue(true);
+        setAnimateClose(false);
+        clearTimeout(setTimeoutID)
+    }, 300); 
+
+  }
+
+  if (onClickBool) {
+    return (
+      <div onClick={toggleOnNavBar} className="NavBar">
+        <span></span>
+        <span></span>
+      </div>
+    );
+  } else {
     
+    return (
+      <>
+        <div onClick={toggleOffNavBar} className="exitBtn">
+            <span></span>
+            <span></span>
+        </div>
+        <div className={`NavBarClicked ${animateClose ? "slideOut" : ""}`}>
+          <div className="linksContainer">
+            <div className="test">
+              <p>Navigation</p>
+              <div className="navLine1"></div>
+            </div>
+            <ul>
+              <li><a href="">Home</a></li>
+              <li><a href="">Work</a></li>
+              <li><a href="">About</a></li>
+              <li><a href="">Contact</a></li>
+            </ul>
 
-    if (onClickBool) {
-        return (
-            <div onClick={toggleNavBar} className="NavBar">
-                <span></span>
-                <span></span>
-                <span></span>
+            <div className="test2">
+              <p>Socials</p>
+              <div className="navLine2"></div>
+              <ul>
+                <li className="insta"></li>
+                <li className="github"></li>
+                <li className="linkedIn"></li>
+              </ul>
             </div>
-        );
-    } else {
-        return (
-            <div className="NavBarClicked">
-                <div className="exitBtn"></div>
-                <div className="linksContainer">
-                    <ul>
-                        <li>
-                            <a href="">Home</a>
-                        </li>
-                        <li>
-                            <a href="">Work</a>
-                        </li>
-                        <li>
-                            <a href="">About</a>
-                        </li>
-                        <li>
-                            <a href="">Contact</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        );
-    }
+          </div>
+        </div>
+      </>
+    );
+  }
 };
